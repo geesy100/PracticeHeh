1f5aeb4c90536e2184424c290511073882a859cc
Paul Gilleece <mybluebucket@hotmail.co.uk>
2015-05-22 15:12:19 +0100

    something or other

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by xubuntu on 21/05/15.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World!");
+        // WHAT WHAT WHAT WHAT WHAT?
+        // HUH HUH HUH HUH HUH?
+    }
+}
